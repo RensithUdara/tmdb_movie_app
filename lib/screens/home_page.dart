5312d4ffa6d7cf555ba541ac90c57990a82ca4af
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-10-27 23:55:35 +0530

    update sizes

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -404,7 +404,7 @@ class _HomePageState extends State<HomePage> {
                                                     const Icon(
                                                       Icons.star,
                                                       color: Colors.amber,
-                                                      size: 16,
+                                                      size: 15,
                                                     )
                                                   ],
                                                 ),
